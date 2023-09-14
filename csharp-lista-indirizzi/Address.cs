ad97e95df62bdae24ad6368ed84931e43715df36
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-14 15:18:11 +0200

    Aggiungere i file di progetto.

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Address
+    {
+        public string Name { get;  private set; }
+        public string Surname { get; private set; }
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string Province { get; private set; }
+        public int Zip {  get; private set; }
+
+        public Address(string name, string surname, string street, string city, string province, int zip) 
+        {
+            this.Name = name;
+            this.Surname = surname; 
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.Zip = zip;
+
+        }
+    }
+
+}
---
+++ csharp-lista-indirizzi/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace csharp_lista_indirizzi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Address> list = new List<Address>();
+
+          
+        }
+    }
+}
